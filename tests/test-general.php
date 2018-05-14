acc2e76c9192ccf147765830da506b5e2c760883
kishoreacharya <kishorems6@gmail.com>
2018-05-14 18:57:37 +0200

    Update test-general.php

--- tests/test-general.php
+++ tests/test-general.php
@@ -7,9 +7,7 @@ use MotorK\{ Rate, Tae, Taeg };
 
 class TestTaeg extends \PHPUnit\Framework\TestCase {
 	
-	/**
-	 * Example from http://www.calcolatoremutui.it/tan-e-taeg/
-	 */
+	
 	public function test_tae() {
 		$obj = Tae::init( 5, 12 );
 
@@ -19,27 +17,5 @@ class TestTaeg extends \PHPUnit\Framework\TestCase {
 		echo $obj;
 	}
 
-	/**
-	 * Example from http://www.calcolatoremutui.it/tan-e-taeg/
-	 */
-	/*public function test_rate() {
-		$obj = Rate::init( 100000, 5, 12, 20 );
-
-		$this->assertEquals( 659.96, round( $obj->calculate(), 2 ) );
-
-		$this->expectOutputString( '659.96 €' );
-		echo $obj;
-	}*/
-
-	/**
-	 * Example from http://www.calcolatoremutui.it/tan-e-taeg/
-	 */
-	/*public function test_taeg() {
-		$obj = Taeg::init( 99000, 5, 661.96, 12, 20 );
-
-		$this->assertEquals( 5.281, round( $obj->calculate(), 3 ) );
-
-		$this->expectOutputString( '5.281 %' );
-		echo $obj;
-	}*/
+	
 }
